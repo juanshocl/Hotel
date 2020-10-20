84f2cc1353349c61c89291a490d4abb0285701ff
Juan Muñoz <cl.jmunoz@gmail.com>
2020-10-20 01:28:46 -0300

    Carga de templates

--- hotel/settings.py
+++ hotel/settings.py
@@ -42,6 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'apps.home',
+    'apps.registration',
+    'apps.booking',
 ]
 
 MIDDLEWARE = [
--- hotel/urls.py
+++ hotel/urls.py
@@ -19,4 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('apps.home.urls')),
+    path('registration/', include('apps.registration.urls')),
+    path('booking/', include('apps.booking.urls')),
 ]
