145382ba92fc8a78eeb9497c328a7bd2c92d4b34
Juan Muñoz <cl.jmunoz@gmail.com>
2020-10-20 01:35:04 -0300

    Update calendar

--- hotel/urls.py
+++ hotel/urls.py
@@ -19,6 +19,8 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('apps.home.urls')),
-    path('registration/', include('apps.registration.urls')),
+    path('from django.contrib import admin
+from . import views
+from django.urls import path/', include('apps.registration.urls')),
     path('booking/', include('apps.booking.urls')),
 ]
